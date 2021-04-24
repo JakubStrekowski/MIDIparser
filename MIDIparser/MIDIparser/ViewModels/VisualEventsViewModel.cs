c48d999750985f3141197cd406c77eab80f59a7f
TrustyJacopo <jakub1403@gmail.com>
2021-04-24 18:48:43 +0200

    Add effects editor layout

---
+++ MIDIparser/MIDIparser/ViewModels/VisualEventsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIDIparser.ViewModels
+{
+    class VisualEventsViewModel : INotifyPropertyChanged
+    {
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChange(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
